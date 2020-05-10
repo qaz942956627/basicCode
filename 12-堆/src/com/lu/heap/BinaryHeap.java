31ed1b3904a613da0a65fa11975e5e0c95da4894
qaz942956627 <942956627@qq.com>
2020-05-11 00:11:05 +0800

    堆

---
+++ 12-堆/src/com/lu/heap/BinaryHeap.java
@@ -0,0 +1,41 @@
+package com.lu.heap;
+
+/**
+ * @author 小卢
+ */
+public class BinaryHeap<E> implements Heap<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public E get() {
+        return null;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E replace(E element) {
+        return null;
+    }
+}
---
+++ 12-堆/src/com/lu/heap/Heap.java
@@ -0,0 +1,21 @@
+package com.lu.heap;
+
+/**
+ * @author 小卢
+ */
+public interface Heap<E> {
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+
+    void add(E element);
+
+    E get();
+
+    E remove();
+
+    E replace(E element);
+}
