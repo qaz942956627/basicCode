52709b2e484f6d70d8094122b493ceeb55047441
qaz942956627 <942956627@qq.com>
2020-05-10 21:50:04 +0800

    反人类for循环版本

--- 算法-01-排序/src/com/lu/BubbleSort.java
+++ 算法-01-排序/src/com/lu/BubbleSort.java
@@ -42,21 +42,12 @@ public class BubbleSort {
 
     public Integer[] bubbleSort3(Integer[] nums) {
         int size = nums.length;
-        int end  = size -1;
-        /*for (int i = 0; i < end; ) {
+        //实际上这个for循环就是一个 sortIndex != -1 的where循环
+        for (int i = 1; i < size; i++) {
 
-            for (int f = 0; f < nums.length; f++) {
-                System.out.print(nums[f]+"->");
-            }
-            System.out.print( "    end"+end+"->");
-            System.out.println();
-        }*/
-
-        int sortIndex;
-        do {
-            // 在数组初始完全有序的时候有用  完全有序 size = 2  i++ = 2 不满足 i < size 循环结束 复杂度 O(n)
-            sortIndex = -1;
-            for (int j = 0; j < end; j++) {
+            // 在数组初始完全有序的时候有用  完全有序 size = -1  i++ = 1 不满足 i < size 循环结束 复杂度 O(n)
+            int sortIndex = -1;
+            for (int j = 0; j < size - i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     Integer tmp = nums[j];
                     nums[j] = nums[j + 1];
@@ -65,13 +56,9 @@ public class BubbleSort {
                     sortIndex = j;
                 }
             }
-            end = sortIndex + 1;
-            for (int f = 0; f < nums.length; f++) {
-                System.out.print(nums[f]+"->");
-            }
-            System.out.print( "    end"+end+"->");
-            System.out.println();
-        } while (sortIndex != -1);
+            size = sortIndex +1;
+            i=0;
+        }
         return nums;
     }
 }
