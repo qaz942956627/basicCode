592a8db5a7f3e5cdf47a54410c0ef3834080a54b
qaz942956627 <942956627@qq.com>
2020-05-10 21:57:52 +0800

    最终where循环版

--- 算法-01-排序/test/com/lu/BubbleSortTest.java
+++ 算法-01-排序/test/com/lu/BubbleSortTest.java
@@ -3,17 +3,28 @@ package com.lu;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 class BubbleSortTest {
 
     @Test
     void bubbleSort() {
-        Integer[] nums = new Integer[]{4, 2, 6, 7, 54, 21, 354, 321, 4, 2, 0, 4, 3, 9, 12, 56, 33};
-        System.out.println(nums.length);
-        Integer[] integers = new BubbleSort().bubbleSort(nums);
+        Integer[] nums = new Integer[]{4, 6, 7, 54, 21, 354, 321,  2, 33,500};
+        //Integer[] nums = new Integer[]{7,5,4,8,20,13,22,23,24};
+        //Arrays.sort(nums, Collections.reverseOrder());
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+"->");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        Integer[] integers = new BubbleSort().bubbleSort3(nums);
         for (int i = 0; i < integers.length; i++) {
             System.out.print(integers[i]+"->");
         }
         System.out.println();
-        System.out.println(nums.length);
+        //System.out.println(nums.length);
     }
 }
