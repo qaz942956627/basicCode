1e8c6aa9d82884d24fdd2e14a962534d3d97c045
qaz942956627 <942956627@qq.com>
2020-05-10 21:27:23 +0800

    冒泡优化3 do while版

--- 算法-01-排序/src/com/lu/BubbleSort.java
+++ 算法-01-排序/src/com/lu/BubbleSort.java
@@ -20,4 +20,58 @@ public class BubbleSort {
         }
         return nums;
     }
+
+    public Integer[] bubbleSort2(Integer[] nums) {
+        int size = nums.length;
+        for (int i = 1; i < size; i++) {
+            boolean flg = true;
+            for (int j = 0; j < size - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    Integer tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    flg = false;
+                }
+            }
+            if (flg) {
+                return nums;
+            }
+        }
+        return nums;
+    }
+
+    public Integer[] bubbleSort3(Integer[] nums) {
+        int size = nums.length;
+        int end  = size -1;
+        /*for (int i = 0; i < end; ) {
+
+            for (int f = 0; f < nums.length; f++) {
+                System.out.print(nums[f]+"->");
+            }
+            System.out.print( "    end"+end+"->");
+            System.out.println();
+        }*/
+
+        int sortIndex;
+        do {
+            // 在数组初始完全有序的时候有用  完全有序 size = 2  i++ = 2 不满足 i < size 循环结束 复杂度 O(n)
+            sortIndex = -1;
+            for (int j = 0; j < end; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    Integer tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    //如果交换了记录一下最后一次交换j的位置
+                    sortIndex = j;
+                }
+            }
+            end = sortIndex + 1;
+            for (int f = 0; f < nums.length; f++) {
+                System.out.print(nums[f]+"->");
+            }
+            System.out.print( "    end"+end+"->");
+            System.out.println();
+        } while (sortIndex != -1);
+        return nums;
+    }
 }
