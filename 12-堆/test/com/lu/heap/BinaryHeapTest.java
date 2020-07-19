dc42f399cd7e4a8e5fd806e1130751992a8c79a8
qaz942956627 <942956627@qq.com>
2020-07-20 00:05:36 +0800

    提交

--- 12-堆/test/com/lu/heap/BinaryHeapTest.java
+++ 12-堆/test/com/lu/heap/BinaryHeapTest.java
@@ -4,9 +4,16 @@ import com.lu.heap.printer.BinaryTreeInfo;
 import com.lu.heap.printer.BinaryTrees;
 import org.junit.jupiter.api.Test;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryHeapTest {
+
+    private int a = 1;
 
     @Test
     void add() {
@@ -23,5 +30,40 @@ class BinaryHeapTest {
         heap.add(23);
         heap.add(11);
         BinaryTrees.print(heap);
+    }
+
+    private  void testThis() {
+        System.out.println(a);
+        testAdd(a);
+        System.out.println(a);
+        Deque<Integer> deque = new LinkedList<>();
+    }
+
+    @Test
+    void testM() {
+        new BinaryHeapTest().testThis();
+    }
+
+    private void testAdd(int a) {
+        a= a+1;
+    }
+
+    @Test
+    void cdlTest() throws InterruptedException {
+        CountDownLatch cdl = new CountDownLatch(10);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                cdl.countDown();
+                System.out.println(Thread.currentThread().getName());
+            }).start();
+        }
+        cdl.await();
+        System.out.println("success");
     }
 }
